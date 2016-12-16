d386a062867ff95b75c56f7bd7c47532382da95f
erick2014 <erickarmandogarcia@hotmail.com>
2016-12-16 18:09:11 -0500

    Change ItemList comp extension

---
+++ src/components/ItemList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Component } from 'react';
+
+class ItemList extends Component {
+    render() {
+      return(
+        <div>hello world</div>
+      )
+    }
+}
+
+export default ItemList;
